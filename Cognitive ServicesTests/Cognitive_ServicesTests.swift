ce2da698dd3fee6cdc7df50c72059014990a0b6d
Vina <spiism@gmail.com>
2019-11-12 09:46:30 +1300

    update testing

---
+++ Cognitive ServicesTests/Cognitive_ServicesTests.swift
@@ -0,0 +1,37 @@
+//
+//  Cognitive_ServicesTests.swift
+//  Cognitive ServicesTests
+//
+//  Created by Vina on 12/11/19.
+//  Copyright © 2019 Unitec. All rights reserved.
+//
+
+import XCTest
+
+class Cognitive_ServicesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure(metrics: [XCTCPUMetric()]) {
+            // Put the code whose CPU performance you want to measure here.
+        }
+    }
+    
+    func testForViewcontroller() {
+            let _ = UIViewController()
+          }
+
+}
